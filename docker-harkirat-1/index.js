da6eaadcb5498ad9eceae3cb1bd9aa16372ae6df
Pushpak-bhoite <bhoitepushpak6@gmail.com>
2025-05-05 01:56:57 +0530

    feat: added remaining files

--- docker-harkirat-1/index.js
+++ docker-harkirat-1/index.js
@@ -4,17 +4,17 @@
 const express = require('express')
 const { default: mongoose } = require('mongoose')
 const app = express()
-const port = 3000
+const port = 3000;
 
 // mongoose.connect('mongodb://localhost:27018/HelloDatabase', {
-// mongoose.connect('mongodb://mongo-container-1/HelloDatabase', {
-// const MONGO_URI= mongodb://mongo-container-1/HelloDatabase
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect('mongodb://mongo-container-1/HelloDatabase', {
+    // const MONGO_URI= mongodb://mongo-container/HelloDatabase
+    // mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
     .then(() => {
-        console.log('Connected to MongoDB');
+        console.log('Connected to MongoDB---');
     })
     .catch((error) => {
         console.error('Error connecting to MongoDB:', error);
@@ -28,16 +28,21 @@ const EntrySchema = new mongoose.Schema({
 
 const Entry = mongoose.model('Entry', EntrySchema);
 
+console.log('---hello-----')
+
 app.get('/', async (req, res) => {
     // res.send('Hello World! This is going to be first docker image')
+    console.log('gggg---')
     try {
-        const entry = new Entry({ text: 'this is an entry ' })
+        const entry = new Entry({ text: `New Entry added on ${new Date().toLocaleString()}` })
         await entry.save();
-        res.send('entry added !')
+        res.json({ status: 'Success', message: 'entry added !', data: entry })
     } catch (error) {
         res.status(500).send('Error occured ')
     }
 })
+
+
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
